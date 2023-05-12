daedbc732730cfeb88a94635cc5c4fb61089a94c
Amanda Mussio <amandamussio@MacBook-Pro-de-Amanda.local>
2023-05-12 14:31:17 -0300

    add firebase

--- base_app/lib/main.dart
+++ base_app/lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:core/app/app_module.dart';
 import 'package:core/app/app_widget.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(ModularApp(module: AppModule(), child: const AppWidget(initialRoute: '/splash'),),);
+void main() async {
+ WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(ModularApp(module: AppModule(), child: const AppWidget(initialRoute: '/'),),);
 }
 
